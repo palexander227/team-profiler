132d50743f7b34b6f117f787668492c814d9c18b
Mandolin Daze <renormalize@gmail.com>
2021-07-18 14:13:36 -0700

    added classes to main script, fixed import problems, testing output

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import inquirer from "inquirer"
 import {managerq, employeeq, internq, engineerq} from "./src/questions.js"
+import Employee from "./lib/Employee.js";
+import Intern from "./lib/Intern.js";
+import Engineer from "./lib/Engineer.js";
+import Manager from "./lib/Manager.js";
+
 const allAnswers = {employees:[]}
 inquirer.prompt(managerq)
       .then(function(answer){
@@ -41,18 +46,29 @@ inquirer.prompt(managerq)
       }
 
       const createEmployee = () => {
-        inquirer.prompt(employeeq).then(answer => allAnswers.employees.push(answer))
-        console.log(allAnswers)
+        inquirer.prompt(employeeq).then(answer => {
+          let temp = new Employee(answer.name, answer.id, answer.email)
+          allAnswers.employees.push(temp)
+          addEmployee()
+        })          
+          
+        
       }
 
       const createEngineer = () => {
-        console.log("creating engineer")
+        inquirer.prompt(employeeq).then(answer => {
+          allAnswers.employees.push(answer)
+          addEmployee()
+        }) 
       }
 
       const createIntern = () => {
-        console.log("creating intern")
+        inquirer.prompt(employeeq).then(answer => {
+          allAnswers.employees.push(answer)
+          addEmployee()
+        }) 
       }
 
       const generateHtml = () => {
-        console.log("creating HTML")
+        console.log(allAnswers)
       }
